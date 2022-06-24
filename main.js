0965b35a5f6b88df181662d705cc7900f3b596c3
wanderer120 <megamanx120@hotmail.com>
2022-06-25 01:33:22 +0800

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+let screenDOM = document.getElementById('screen');
+let screen = screenDOM.getContext("2d");
+
+screen.font = "10px Arial";
+screen.fillText("\u{25A0}\u{25A0}\u{25A0}\u{25A0}\u{25A0}", 10, 50);
+screen.fillText("\u{25A0}\u{25A0}\u{25A0}\u{25A0}\u{25A0}", 10, 56);
+screen.fillText("\u{25A0}\u{25A0}\u{25A0}\u{25A0}\u{25A0}", 10, 62);
+screen.fillText("\u{25A0}\u{25A0}\u{25A0}\u{25A0}\u{25A0}", 10, 68);
+screen.fillText("\u{25A0}\u{25A0}\u{25A0}\u{25A0}\u{25A0}", 10, 74);
