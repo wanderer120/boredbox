7fa49f5678425fc6587ff774bdfdd5dcbb19f477
wanderer120 <megamanx120@hotmail.com>
2022-07-16 23:22:49 +0800

    change back monster sprite to main

--- MonsterScreen.js
+++ MonsterScreen.js
@@ -1,65 +1,16 @@
 class MonsterScreen{
     shouldFlipImage = false;
-    monsterWalk1Buffer = new ArrayBuffer(32);
-    monsterWalk2Buffer = new ArrayBuffer(32);
-    monsterWalk1Pixel = new Uint16Array(this.monsterWalk1Buffer);
-    monsterWalk2Pixel = new Uint16Array(this.monsterWalk2Buffer);
+    monsterWalk1Pixel;
+    monsterWalk2Pixel;
     monPosX;
     monFace;
-    monsterPixel
-    constructor(monPosX,monFace){
+    monsterPixel;
+
+    constructor(monsterWalk1Pixel, monsterWalk2Pixel, monPosX,monFace){
         this.monPosX = monPosX;
         this.monFace = monFace;
-        this.monsterWalk1Pixel[0]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[1]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[2]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[3]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[4]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[5]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[6]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[7]  = 0b0000000000000000;
-        this.monsterWalk1Pixel[8]  = 0b0000110001100000;
-        this.monsterWalk1Pixel[9]  = 0b0000101110100000;
-        this.monsterWalk1Pixel[10] = 0b0001000000010000;
-        this.monsterWalk1Pixel[11] = 0b0010010001001000;
-        this.monsterWalk1Pixel[12] = 0b0010010001001000;
-        this.monsterWalk1Pixel[13] = 0b0010000100001000;
-        this.monsterWalk1Pixel[14] = 0b0010000000001000;
-        this.monsterWalk1Pixel[15] = 0b0001111111110000;
-
-        // this.monsterWalk1Pixel[0]  = 0b0000000000000000;
-        // this.monsterWalk1Pixel[1]  = 0b0000001111100000;
-        // this.monsterWalk1Pixel[2]  = 0b0000010000010000;
-        // this.monsterWalk1Pixel[3]  = 0b0000110110001000;
-        // this.monsterWalk1Pixel[4]  = 0b0011000011001000;
-        // this.monsterWalk1Pixel[5]  = 0b0100000111000100;
-        // this.monsterWalk1Pixel[6]  = 0b0111100000000100;
-        // this.monsterWalk1Pixel[7]  = 0b0010000000000100;
-        // this.monsterWalk1Pixel[8]  = 0b0001111100001000;
-        // this.monsterWalk1Pixel[9]  = 0b0000110000101000;
-        // this.monsterWalk1Pixel[10] = 0b0001010001000100;
-        // this.monsterWalk1Pixel[11] = 0b0001110001110100;
-        // this.monsterWalk1Pixel[12] = 0b0000011000010010;
-        // this.monsterWalk1Pixel[13] = 0b0001100111100010;
-        // this.monsterWalk1Pixel[14] = 0b0010100101010101;
-        // this.monsterWalk1Pixel[15] = 0b0011111101111111;
-
-        this.monsterWalk2Pixel[0]  = 0b0000000000000000;
-        this.monsterWalk2Pixel[1]  = 0b0000000000000000;
-        this.monsterWalk2Pixel[2]  = 0b0000000000000000;
-        this.monsterWalk2Pixel[3]  = 0b0000000000000000;
-        this.monsterWalk2Pixel[4]  = 0b0000000000000000;
-        this.monsterWalk2Pixel[5]  = 0b0000000000000000;
-        this.monsterWalk2Pixel[6]  = 0b0000011111000000;
-        this.monsterWalk2Pixel[7]  = 0b0000100000110000;
-        this.monsterWalk2Pixel[8]  = 0b0000010000001000;
-        this.monsterWalk2Pixel[9]  = 0b0000001000100100;
-        this.monsterWalk2Pixel[10] = 0b0000000101101010;
-        this.monsterWalk2Pixel[11] = 0b0000000100000010;
-        this.monsterWalk2Pixel[12] = 0b0001010100000110;
-        this.monsterWalk2Pixel[13] = 0b0001111000000100;
-        this.monsterWalk2Pixel[14] = 0b0001000000001000;
-        this.monsterWalk2Pixel[15] = 0b0000111111110000;
+        this.monsterWalk1Pixel = monsterWalk1Pixel;
+        this.monsterWalk2Pixel = monsterWalk2Pixel;
 
         this.monsterPixel = this.monsterWalk1Pixel;
     }
--- main.js
+++ main.js
@@ -10,7 +10,63 @@ let monFace = 0;
 // screenUI.fill();
 // screenUI.fillStyle = "black";
 
-let monsterScreen = new MonsterScreen(monPosX,monFace);
+let monsterWalk1Buffer = new ArrayBuffer(32);
+let monsterWalk2Buffer = new ArrayBuffer(32);
+let monsterWalk1Pixel = new Uint16Array(monsterWalk1Buffer);
+let monsterWalk2Pixel = new Uint16Array(monsterWalk2Buffer);
+
+monsterWalk1Pixel[0]  = 0b0000000000000000;
+monsterWalk1Pixel[1]  = 0b0000000000000000;
+monsterWalk1Pixel[2]  = 0b0000000000000000;
+monsterWalk1Pixel[3]  = 0b0000000000000000;
+monsterWalk1Pixel[4]  = 0b0000000000000000;
+monsterWalk1Pixel[5]  = 0b0000000000000000;
+monsterWalk1Pixel[6]  = 0b0000000000000000;
+monsterWalk1Pixel[7]  = 0b0000000000000000;
+monsterWalk1Pixel[8]  = 0b0000110001100000;
+monsterWalk1Pixel[9]  = 0b0000101110100000;
+monsterWalk1Pixel[10] = 0b0001000000010000;
+monsterWalk1Pixel[11] = 0b0010010001001000;
+monsterWalk1Pixel[12] = 0b0010010001001000;
+monsterWalk1Pixel[13] = 0b0010000100001000;
+monsterWalk1Pixel[14] = 0b0010000000001000;
+monsterWalk1Pixel[15] = 0b0001111111110000;
+
+// monsterWalk1Pixel[0]  = 0b0000000000000000;
+// monsterWalk1Pixel[1]  = 0b0000001111100000;
+// monsterWalk1Pixel[2]  = 0b0000010000010000;
+// monsterWalk1Pixel[3]  = 0b0000110110001000;
+// monsterWalk1Pixel[4]  = 0b0011000011001000;
+// monsterWalk1Pixel[5]  = 0b0100000111000100;
+// monsterWalk1Pixel[6]  = 0b0111100000000100;
+// monsterWalk1Pixel[7]  = 0b0010000000000100;
+// monsterWalk1Pixel[8]  = 0b0001111100001000;
+// monsterWalk1Pixel[9]  = 0b0000110000101000;
+// monsterWalk1Pixel[10] = 0b0001010001000100;
+// monsterWalk1Pixel[11] = 0b0001110001110100;
+// monsterWalk1Pixel[12] = 0b0000011000010010;
+// monsterWalk1Pixel[13] = 0b0001100111100010;
+// monsterWalk1Pixel[14] = 0b0010100101010101;
+// monsterWalk1Pixel[15] = 0b0011111101111111;
+
+monsterWalk2Pixel[0]  = 0b0000000000000000;
+monsterWalk2Pixel[1]  = 0b0000000000000000;
+monsterWalk2Pixel[2]  = 0b0000000000000000;
+monsterWalk2Pixel[3]  = 0b0000000000000000;
+monsterWalk2Pixel[4]  = 0b0000000000000000;
+monsterWalk2Pixel[5]  = 0b0000000000000000;
+monsterWalk2Pixel[6]  = 0b0000011111000000;
+monsterWalk2Pixel[7]  = 0b0000100000110000;
+monsterWalk2Pixel[8]  = 0b0000010000001000;
+monsterWalk2Pixel[9]  = 0b0000001000100100;
+monsterWalk2Pixel[10] = 0b0000000101101010;
+monsterWalk2Pixel[11] = 0b0000000100000010;
+monsterWalk2Pixel[12] = 0b0001010100000110;
+monsterWalk2Pixel[13] = 0b0001111000000100;
+monsterWalk2Pixel[14] = 0b0001000000001000;
+monsterWalk2Pixel[15] = 0b0000111111110000;
+
+let monsterScreen = new MonsterScreen(monsterWalk1Pixel, monsterWalk2Pixel, monPosX,monFace);
 
 setInterval(()=>{
     let screenArr = [];
